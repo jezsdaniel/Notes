dedcf019a197896454b008318f19d79f5bbe8f60
jezsdaniel <jadendaniel7@gmail.com>
2021-05-19 14:25:09 -0400

    Improve notes methods

--- src/App.js
+++ src/App.js
@@ -8,46 +8,45 @@ import NotesStack from './routes';
 const key = 'notes';
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const getDbNotes = () => {
+    if (!MMKV.getString(key)) {
+      return [];
+    }
 
-  useEffect(() => {
     try {
       const dbNotes = MMKV.getString(key);
       const objectNotes = JSON.parse(dbNotes);
-      setNotes(objectNotes);
+      return objectNotes;
     } catch (e) {
       console.log('Error from MMKV: ' + e);
+      return [];
     }
-  }, []);
+  };
+
+  const [notes, setNotes] = useState(getDbNotes);
+
+  useEffect(() => {
+    MMKV.set(key, JSON.stringify(notes));
+  }, [notes]);
 
   const addNote = note => {
-    const tempNotes = notes;
-    tempNotes.push(note);
-
-    setNotes([...tempNotes]);
-    MMKV.set(key, JSON.stringify(tempNotes));
+    setNotes([...notes, note]);
   };
 
   const updateNote = note => {
     const index = notes.findIndex(item => {
       return item.id === note.id;
     });
-    const tempNotes = [
-      ...notes.slice(0, index),
-      note,
-      ...notes.slice(index + 1),
-    ];
-    setNotes([...tempNotes]);
-    MMKV.set(key, JSON.stringify(tempNotes));
+
+    setNotes([...notes.slice(0, index), note, ...notes.slice(index + 1)]);
   };
 
   const deleteNote = note => {
     const index = notes.findIndex(item => {
       return item.id === note.id;
     });
-    const tempNotes = [...notes.slice(0, index), ...notes.slice(index + 1)];
-    setNotes([...tempNotes]);
-    MMKV.set(key, JSON.stringify(tempNotes));
+
+    setNotes([...notes.slice(0, index), ...notes.slice(index + 1)]);
   };
 
   return (
