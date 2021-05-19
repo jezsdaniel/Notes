3bb286a45168fb0ed1568f5e4354e012ca13f912
jezsdaniel <jadendaniel7@gmail.com>
2021-05-19 14:36:29 -0400

    Added necesary import react-native-gesture-handler

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
 import {MMKV} from 'react-native-mmkv';
 import {NavigationContainer} from '@react-navigation/native';
